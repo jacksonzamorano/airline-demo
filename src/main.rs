768982cd50a1897e88a9ebd51d7f4035f1978560
Jackson Zamorano <jackson@jacksonzamorano.com>
2023-01-18 14:22:55 -0700

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+use airline::{Request, RequestType, Response, Route, Server};
+use assets::Assets;
+pub mod assets;
+
+struct ServerEnvironment {
+    database: Database,
+}
+struct Database;
+
+fn setup() -> ServerEnvironment {
+    return ServerEnvironment {
+        database: Database {},
+    };
+}
+
+fn index(_req: &Request, res: &mut Response, _data: &ServerEnvironment) {
+    res.send_string(Assets::index());
+}
+
+fn main() {
+    let mut server = Server::new(3000, setup);
+    server.register(Route::create("", RequestType::Get, index));
+    server.start();
+}
